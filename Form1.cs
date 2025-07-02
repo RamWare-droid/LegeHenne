f2bff0c17f31230ea4ba3f0b4cb231b49da0d57a
paul erstling <erstlingpaul@outlook.com>
2025-07-02 15:17:36 +0200

    This version creates cosinus style csv files Mit Punkt als Trennzeichen fuer Nachkommastellen

--- Form1.cs
+++ Form1.cs
@@ -10,6 +10,40 @@ namespace LegeHenne
             txtPath.Text = "kolophonium.csv";
         }
 
+        private string createPulseForm(int hour, int minute)
+        {
+            // Hilfsroutine zur Erzeugung eines Pulses in Abhaengigkeit der Zeit.
+            string strOpen;
+
+            if (hour == 10 && (minute >= 20 && minute <= 30))
+                strOpen = "202.000\t";
+            else
+                strOpen = "200.000\t";
+            return strOpen;
+        }
+        private string createCosForm(int hour, int minute)
+        {
+            // Hilfsroutine zur Erzeugung eines cosinus in Abhaengigkeit der Zeit.
+
+            int maxMinutes = 24 * 60; // Anzahl Minuten im 24 h Tag.
+            int minutes;
+            double angle;
+            double erg;
+            string strOpen;
+
+            minutes = 60 * hour + minute;
+            angle = 2 * Math.PI * minutes / maxMinutes;
+
+            erg = Math.Cos(angle); // Schwankt zwischen -1 und +1
+
+            // Werte sollen schwanken zwischen 202 und 198, Amplitude == 2
+            erg *= 2 ;
+            erg += 200;
+
+            //´strOpen = erg.ToString() + ".000\t"; // 3 Nachkommastellen und Tab.
+            strOpen = Convert.ToString(erg, System.Globalization.CultureInfo.InvariantCulture) + "\t";
+            return strOpen;
+        }
         private void cmdReadFromFile_Click(object sender, EventArgs e)
         {
             string ueberschrift = "<DATE>\t<TIME>\t<OPEN>\t<HIGH>\t<LOW>\t<CLOSE>\t<TICKVOL>\t<VOL>\t<SPREAD>";
@@ -50,15 +84,14 @@ namespace LegeHenne
                         sb.Append(strMinute).Append(':');
                         sb.Append(strSecond).Append('\t');
 
-                        if (hour == 10 && (minute >= 20 && minute <= 30))
-                            strOpen = "202.000\t";
-                        else
-                            strOpen = "200.000\t";
+                        // strOpen = createPulseForm( hour, minute );
+                        strOpen = createCosForm(hour, minute);
 
                         for (int i = 0; i < 4; i++)
                             sb.Append(strOpen);
+                        // sb.Append("280\t0\t30"); Fuer Spread == 30
+                        sb.Append("280\t0\t00"); // Fuer Spread == 00
 
-                        sb.Append("280\t0\t30");
                         list1.Add(sb.ToString());
                         sb.Clear();
                     } // minute
